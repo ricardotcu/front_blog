2dae27fc7c10500ecb390be3a0a3158d45b14c84
Ricardo <ricardotcu10@gmail.com>
2020-07-14 06:44:11 -0300

    initial

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+  
+export class HomeComponent implements OnInit {
+  title = 'home';	
+  readonly apiURL : string;
+  public rota: Router;
+  public resumo: any;
+
+  constructor(private http : HttpClient, private r: Router){
+    this.apiURL = 'https://blog-memes.herokuapp.com';
+    this.rota = r;
+  }
+
+  ngOnInit(): void {
+    this.http.get(`${this.apiURL}/home`)
+      .subscribe(result => {
+        this.resumo = result;
+        console.log(this.resumo);
+      });
+  }
+
+}
