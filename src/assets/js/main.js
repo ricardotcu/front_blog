529eadf738a6ebb650a18be3bcf4507e3ab59632
ricardotcu10@gmail.com <ricardotcu10@gmail.com>
2020-07-15 12:23:28 -0300

    ajuste17

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -3,24 +3,7 @@ $(function(){
 	'use strict';
 
 	var carousel  = function() {
-		$('.owl-carousel').owlCarousel({
-			loop: true,
-			margin: 10,
-			nav: true,
-			stagePadding: 5,
-			navText: ['<span class="icon-chevron-left">', '<span class="icon-chevron-right">'],
-			responsive:{
-				0:{
-					items: 1
-				},
-				600:{
-					items: 2
-				},
-				1000:{
-					items: 3
-				}
-			}
-		});
+		jQuery("#owl-demo").owlCarousel({});
 	}
 	carousel();
 
