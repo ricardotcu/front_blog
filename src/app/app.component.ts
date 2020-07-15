316955836ca93fcd886f1b9e81808b883fcf7315
ricardotcu10@gmail.com <ricardotcu10@gmail.com>
2020-07-15 17:11:43 -0300

    ajuste62

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontBlog';
+  readonly apiURL : string;
+  public rota: Router;
+  public resumo: any;
+
+  constructor(private http : HttpClient, private r: Router){
+    this.apiURL = 'https://blog-memes.herokuapp.com';
+    this.rota = r;
+  }
+
+  ngOnInit(): void {
+    this.http.get(`${this.apiURL}/home`)
+      .subscribe(result => {
+        this.resumo = result;
+        console.log(this.resumo);
+      });
+  }
 }
